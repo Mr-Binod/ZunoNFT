177bd57acc54ef78c332b3db04b9bf582694eab0
blackorion <104806177+Mr-Binod@users.noreply.github.com>
2025-11-13 19:43:22 +0900

    updated db username

--- zunobackend/src/app.module.ts
+++ zunobackend/src/app.module.ts
@@ -21,16 +21,19 @@ import { UserNftEntity } from './contracts/entities/user-nft.entity';
     ConfigModule.forRoot({
       isGlobal: true, // makes ConfigService available app-wide
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: process.env.DATABASE_NAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: 'b3project',
-      entities: [SmartAccInfoEntity, UserNftEntity, NftUriEntity, SellNftEntity], // ⬅️ Register multiple entities
-      synchronize: false,
-      // dropSchema: true 
+   TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DATABASE_HOST', 'localhost'), // env 파일에 host가 없다면 localhost 기본값
+        port: configService.get<number>('DB_PORT', 3306), // 포트는 env에 없다면 기본값 3306 사용
+        username: configService.get<string>('DATABASE_NAME'), // process.env.DATABASE_NAME 대신 사용
+        password: configService.get<string>('DATABASE_PASSWORD'), // process.env.DATABASE_PASSWORD 대신 사용
+        database: 'b3project',
+        entities: [SmartAccInfoEntity, UserNftEntity, NftUriEntity, SellNftEntity],
+        synchronize: false,
+      }),
+      inject: [ConfigService], // ConfigService를 주입합니다.
     }),
     // SequelizeModule.forRoot({
     //   dialect: 'mysql', // or 'postgres', etc.
@@ -52,4 +55,5 @@ import { UserNftEntity } from './contracts/entities/user-nft.entity';
   providers: [AppService],
 })
 export class AppModule { }
+
   
